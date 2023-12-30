8bd520702f56f190a4a5934a8a9920beebf2b988
Samy Cheklat <samycheklat@hotmail.fr>
2023-12-29 20:46:53 -0500

    Doc strings
    added doc strings instead of comments

--- gatekeeper/app.py
+++ gatekeeper/app.py
@@ -6,7 +6,6 @@ from sshtunnel import SSHTunnelForwarder
 
 app = Flask(__name__)
 
-# Initialize AWS EC2 client
 ec2_client = client(
     service_name='ec2',
     region_name="us-east-1",
@@ -15,11 +14,18 @@ ec2_client = client(
 )
 
 def get_public_ip(keyword):
-    # Get information about EC2 instances
+    """
+    Get information about the running EC2 instances with a specified keyword in the 'Name' tag.
+
+    Args:
+    - keyword (str): Keyword to search for in the 'Name' tag.
+
+    Returns:
+    - dict: Dictionary containing 'Name' and 'PublicDnsName' of the matched instance.
+    """
     response = ec2_client.describe_instances()
     instance_infos = {}
 
-    # Loop through reservations and instances to find the one with the specified keyword
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
             if instance["State"]['Name'] == 'running':
@@ -31,28 +37,49 @@ def get_public_ip(keyword):
     return instance_infos
 
 def establish_tunnel():
-    # Get the public IP of the trusted host instance
+    """
+    Establish an SSH tunnel to a trusted host instance.
+
+    Returns:
+    - SSHTunnelForwarder: An SSHTunnelForwarder object representing the established tunnel.
+    """
     trusted_host_public_ip = get_public_ip("Trusted Host").get('PublicDnsName')
-    
-    # Create an SSH tunnel to the trusted host instance
     return SSHTunnelForwarder((trusted_host_public_ip, 22), ssh_username="ubuntu", ssh_pkey="my_key.pem", remote_bind_address=(trusted_host_public_ip, 80))
      
 def send_request_to_trusted_host(query_type, sql_query):
-    # Get the public IP of the trusted host instance
+    """
+    Send an HTTP GET request to a trusted host instance through an SSH tunnel.
+
+    Args:
+    - query_type (str): Type of SQL query to execute.
+    - sql_query (str): SQL query to execute.
+
+    Returns:
+    - str: Response text from the trusted host.
+    """
     trusted_host_public_ip = get_public_ip("Trusted Host").get('PublicDnsName')
     
-    # Use the established SSH tunnel to send a request to the trusted host
     with establish_tunnel():
         return requests.get(f"http://{trusted_host_public_ip}/query?query_type={query_type}&query={sql_query}").text
 
 @app.route('/health', methods=['GET'])
 def health():
-    # Endpoint to check the health status of the application
+    """
+    Endpoint to check the health status of the application.
+
+    Returns:
+    - JSON: JSON response indicating the health status.
+    """
     return jsonify({'status': 'gatekeeper healthy'})
 
 @app.route('/query', methods=['GET'])
 def query():
-    # Get query_type parameter from the request
+    """
+    Endpoint to execute SQL queries through an SSH tunnel to a trusted host instance.
+
+    Returns:
+    - str: Response text from the trusted host.
+    """
     query_type = request.args.get('query_type')
 
     if query_type is None:
@@ -60,23 +87,18 @@ def query():
     elif query_type not in ['direct_hit', 'random', 'customized']:
         return jsonify({'error': 'Invalid query type'}), 400
     
-    # Get SQL query parameter from the request
     sql_query = request.args.get('query')
 
-    # Check if SQL query is provided
     if not sql_query:
         return jsonify({'error': 'No SQL query provided'}), 400
 
     try:
-        # Execute SQL query through the established SSH tunnel
         result = send_request_to_trusted_host(query_type, sql_query)
         return result
     except Exception as e:
         return jsonify({"error": str(e)})
     finally:
-        # Close the SSH tunnel
         establish_tunnel().close()
 
 if __name__ == "__main__":
-    # Run the Flask application on 0.0.0.0:80
     app.run(debug=True, host='0.0.0.0', port=80)
--- proxy/app.py
+++ proxy/app.py
@@ -10,7 +10,6 @@ from sshtunnel import SSHTunnelForwarder
 
 app = Flask(__name__)
 
-# AWS EC2 client configuration
 client = boto3.client(
     service_name='ec2',
     region_name="us-east-1",
@@ -18,11 +17,20 @@ client = boto3.client(
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
 )
 
-used_manager_ip =''
+used_manager_ip = ''
 used_worker_ip = ''
 
-# Function to retrieve public IP addresses of EC2 instances with a specific keyword in their name
+
 def get_instances_public_ips(keyword):
+    """
+    Get information about the running EC2 instances with a specified keyword in the 'Name' tag.
+
+    Args:
+    - keyword (str): Keyword to search for in the 'Name' tag.
+
+    Returns:
+    - list: List of dictionaries containing 'Name' and 'PublicIpAddress' of matched instances.
+    """
     response = client.describe_instances()
     instances_infos = []
 
@@ -37,60 +45,113 @@ def get_instances_public_ips(keyword):
 
     return instances_infos
 
-# Function to retrieve the public IP address of the manager instance
+
 def get_manager_public_ip():
+    """
+    Retrieve the public IP address of the manager instance.
+
+    Returns:
+    - dict: Dictionary containing 'Name' and 'PublicIpAddress' of the manager instance.
+    """
     manager_infos = {}
     instances_infos = get_instances_public_ips("Manager")
 
     for instance_info in instances_infos:
         manager_infos = instance_info
-        break 
+        break
 
-    return manager_infos  
+    return manager_infos
 
-# Function to measure the ping time to a worker instance
-def measure_worker_ping_time(worker_ip_adress, timeout=2):
+
+def measure_worker_ping_time(worker_ip_address, timeout=2):
+    """
+    Measure the ping time to a worker instance.
+
+    Args:
+    - worker_ip_address (str): Public IP address of the worker instance.
+    - timeout (int): Timeout for the ping operation.
+
+    Returns:
+    - float: Ping time to the worker instance.
+    """
     try:
-        # Measure ping time using the ping3 library
-        response = ping3.ping(worker_ip_adress, timeout=timeout)
-        
+        response = ping3.ping(worker_ip_address, timeout=timeout)
+
         if response is not None:
             return response
     except Exception as e:
-        print(f"Error while pinging {worker_ip_adress}: {e}")
-    
-    # Return a high value if ping fails
+        print(f"Error while pinging {worker_ip_address}: {e}")
+
     return float('inf')
 
-# Function to select the worker instance with the least response time
+
 def select_worker_with_least_response_time(workers):
+    """
+    Select the worker instance with the least response time.
+
+    Args:
+    - workers (list): List of worker instances.
+
+    Returns:
+    - dict: Dictionary containing 'Name' and 'PublicIpAddress' of the selected worker instance.
+    """
     min_ping_time = float('inf')
     selected_worker = None
-    
+
     for worker in workers:
         worker_ip_address = worker['PublicIpAddress']
         ping_time = measure_worker_ping_time(worker_ip_address)
-        
+
         if ping_time < min_ping_time:
             min_ping_time = ping_time
             selected_worker = worker
-    
+
     return selected_worker
 
-# Function to get a random worker instance
+
 def get_random_worker(workers):
+    """
+    Get a random worker instance.
+
+    Args:
+    - workers (list): List of worker instances.
+
+    Returns:
+    - dict: Dictionary containing 'Name' and 'PublicIpAddress' of the selected worker instance.
+    """
     if not workers:
         return None
     return random.choice(workers)
 
-# Function to establish an SSH tunnel to a worker instance
+
 def establish_tunnel(worker_ip, manager_ip):
+    """
+    Establish an SSH tunnel to a worker instance.
+
+    Args:
+    - worker_ip (str): Public IP address of the worker instance.
+    - manager_ip (str): Public IP address of the manager instance.
+
+    Returns:
+    - SSHTunnelForwarder: An SSHTunnelForwarder object representing the established tunnel.
+    """
     app.logger.info(f"Manager IP: {manager_ip}")
     app.logger.info(f"Worker IP: {worker_ip}")
     return SSHTunnelForwarder(worker_ip, ssh_username="ubuntu", ssh_pkey="my_key.pem", remote_bind_address=(manager_ip, 3306))
 
-# Function to execute an SQL query through the established SSH tunnel
-def execute_sql_query(tunnel, query,manager_ip):
+
+def execute_sql_query(tunnel, query, manager_ip):
+    """
+    Execute an SQL query through the established SSH tunnel.
+
+    Args:
+    - tunnel (SSHTunnelForwarder): An SSHTunnelForwarder object representing the established tunnel.
+    - query (str): SQL query to execute.
+    - manager_ip (str): Public IP address of the manager instance.
+
+    Returns:
+    - any: Result of the SQL query.
+    """
     with tunnel:
         conn = pymysql.connect(
             host=manager_ip,
@@ -108,40 +169,68 @@ def execute_sql_query(tunnel, query,manager_ip):
 
     return result
 
-# Function to establish an SSH tunnel to the manager instance
+
 def direct_hit():
+    """
+    Establish an SSH tunnel to the manager instance.
+
+    Returns:
+    - SSHTunnelForwarder: An SSHTunnelForwarder object representing the established tunnel.
+    """
     manager_ip = get_manager_public_ip().get('PublicIpAddress')
     tunnel = establish_tunnel(manager_ip, manager_ip)
     return tunnel
 
-# Function to establish an SSH tunnel to a random worker instance
+
 def random_node():
+    """
+    Establish an SSH tunnel to a random worker instance.
+
+    Returns:
+    - SSHTunnelForwarder: An SSHTunnelForwarder object representing the established tunnel.
+    """
     workers = get_instances_public_ips("Worker")
     random_worker_ip = get_random_worker(workers).get('PublicIpAddress')
     manager_ip = get_manager_public_ip().get('PublicIpAddress')
     tunnel = establish_tunnel(random_worker_ip, manager_ip)
     return tunnel
 
-# Function to establish an SSH tunnel to the fastest worker instance
+
 def customized_node():
+    """
+    Establish an SSH tunnel to the fastest worker instance.
+
+    Returns:
+    - SSHTunnelForwarder: An SSHTunnelForwarder object representing the established tunnel.
+    """
     workers = get_instances_public_ips("Worker")
     fastest_worker_ip = select_worker_with_least_response_time(workers).get('PublicIpAddress')
     manager_ip = get_manager_public_ip().get('PublicIpAddress')
     tunnel = establish_tunnel(fastest_worker_ip, manager_ip)
     return tunnel
 
-# Route to check the health of the proxy
+
 @app.route("/health", methods=['GET'])
 def health():
+    """
+    Route to check the health of the proxy.
+
+    Returns:
+    - JSON: JSON response indicating the health status.
+    """
     return jsonify({"status": "proxy healthy"})
 
-# Route to handle SQL queries based on the query_type parameter
+
 @app.route('/query', methods=['GET'])
 def query():
-    # Get query_type parameter from the request
+    """
+    Route to handle SQL queries based on the query_type parameter.
+
+    Returns:
+    - JSON: JSON response containing the result of the SQL query and additional information.
+    """
     query_type = request.args.get('query_type')
 
-    # Determine the type of query based on the query_type parameter
     if query_type == 'direct_hit':
         tunnel = direct_hit()
     elif query_type == 'random':
@@ -150,29 +239,25 @@ def query():
         tunnel = customized_node()
     else:
         return jsonify({'error': 'Invalid query type'}), 400
-    
-    # Get SQL query parameter from the request
+
     sql_query = request.args.get('query')
 
-    # Check if SQL query is provided
     if not sql_query:
         return jsonify({'error': 'No SQL query provided'}), 400
 
-    # Get the manager's public IP address
     manager_ip = get_manager_public_ip().get('PublicIpAddress')
 
     try:
-        # Execute SQL query through the established SSH tunnel
         result = execute_sql_query(tunnel, sql_query, manager_ip)
         return jsonify(
-            {"Message": f"Query of type '{query_type}' was executed successfully", "Query Result": result, "Manager IP": manager_ip, "Used worker IP": tunnel.ssh_host})
+            {"Message": f"Query of type '{query_type}' was executed successfully", "Query Result": result,
+             "Manager IP": manager_ip, "Used worker IP": tunnel.ssh_host})
     except Exception as e:
         return jsonify({"error": str(e)})
     finally:
-        # Close the SSH tunnel
         tunnel.close()
 
-# Run the Flask application
+
 if __name__ == '__main__':
     app.logger.setLevel(logging.INFO)
     app.run(debug=True, host='0.0.0.0', port=80)
